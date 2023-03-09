7f57f16a75613492f7572a9371431dd8587edc5d
fchrstou <filippos.christou@ikr.uni-stuttgart.de>
2023-03-09 20:02:03 +0100

    Added rotatecoords function

--- src/intentplot.jl
+++ src/intentplot.jl
@@ -85,3 +85,39 @@ function _plotablesubdagnodes!(vs, dag, node, parnode, goingout)
         end
     end
 end
+
+
+"""
+    rotatecoords(xs::AbstractVector, ys::AbstractVector, paths::AbstractDict, θ)
+
+Rotate coordinates `xs`, `ys` and paths `paths` by `angle`
+"""
+function rotatecoords(xs::AbstractVector, ys::AbstractVector, paths::AbstractDict, θ)
+    # rotation matrix
+    r = [cos(θ) -sin(θ); sin(θ) cos(θ)]
+    points = vcat.(xs, ys)
+    newpoints = [r * pointvec for pointvec in points]
+    newpaths = Dict(k => let newpath = [r * pointvec for pointvec in vcat.(v...)]
+             (getindex.(newpath, 1), getindex.(newpath, 2))
+         end
+         for (k,v) in paths)
+    return getindex.(newpoints, 1), getindex.(newpoints, 2), newpaths
+end
+
+"""
+    rotatecoords!(xs::AbstractVector, ys::AbstractVector, paths::AbstractDict, θ)
+
+Rotate coordinates `xs`, `ys` and paths `paths` by `angle`
+"""
+function rotatecoords!(xs::AbstractVector, ys::AbstractVector, paths::AbstractDict, θ)
+    # rotation matrix
+    r = [cos(θ) -sin(θ); sin(θ) cos(θ)]
+    points = vcat.(xs, ys)
+    newpoints = [r * pointvec for pointvec in points]
+    xs .= getindex.(newpoints, 1)
+    ys .= getindex.(newpoints, 2)
+    for (k,v) in paths
+        newpath = [r * pointvec for pointvec in vcat.(v...)]
+        paths[k] = (getindex.(newpath, 1), getindex.(newpath, 2))
+    end
+end
